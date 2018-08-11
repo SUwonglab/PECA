84af9ab6b416e7a45b7681843f2020c941988458
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2018-08-10 17:53:14 -0700

    Add files via upload

--- run_PECA.sh
+++ run_PECA.sh
@@ -53,7 +53,7 @@ rm read.bed
 
 echo step 4: Prior....
 bedtools intersect -a ../../Data/Promoter_100k_${genome}.bed  -b region.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{print $5,$6,$7,$4,$3-100000-$6}'|sed 's/-//g'|sort -k1,1 -k2,2n >peak_gene_100k.bed
-bedtools intersect -a peak_gene_100k.bed -b ../../Data/RE_gene_corr_${genome}.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{if ($4==$9) print $1,$2,$3,$4,$5,$10}'|sed 's/\t/\_/1'|sed 's/\t/\_/1'>peak_gene_100k_corr.bed
+bedtools intersect -a peak_gene_100k.bed -b ../../Prior/RE_gene_corr_${genome}.bed -wa -wb -sorted|awk 'BEGIN{OFS="\t"}{if ($4==$9) print $1,$2,$3,$4,$5,$10}'|sed 's/\t/\_/1'|sed 's/\t/\_/1'>peak_gene_100k_corr.bed
 rm peak_gene_100k.bed
 
 echo step 5: Network....
