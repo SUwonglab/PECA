3060f1db462a73f93f180450b2b382b61863496f
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2018-10-12 10:11:10 -0700

    Update run_PECA_compare_dif_multiple.sh

--- run_PECA_compare_dif_multiple.sh
+++ run_PECA_compare_dif_multiple.sh
@@ -2,17 +2,22 @@
 
 Group1=stem1
 Group2=stem2
+organism=human
 # Examples
 # Group1=Control
 # Group2=Patients
+# organism=human
 
 mkdir ./Results/CompareGroup_${Group1}_${Group2}
 cd ./Results/CompareGroup_${Group1}_${Group2}
 cp ../../scr/FindSubNetwork_bi1.m ./.
+cp ../../scr/FindSubNetwork_bi2.m ./.
 cp ../../scr/dif_test.m ./.
 cp ../../${Group1} ./.
 cp ../../${Group2} ./.
-sed "s/Sample1/${Group1}/g" ../../scr/PECA_dif_net_multiple.m|sed "s/Sample2/${Group2}/g" > PECA_dif_net_multiple.m
+sed "s/Sample1/${Group1}/g" ../../scr/PECA_dif_net_multiple.m|sed "s/Sample2/${Group2}/g"|sed "s/organism/${organism}/g" > PECA_dif_net_multiple.m
 module load matlab
 matlab -nodisplay -nosplash -nodesktop -r "PECA_dif_net_multiple; exit"
+cat *_specific_network.txt|grep -v log10P_Regulation|cut -f 1-3|awk '{$3=$3>0?1:-1}1'|tr ' ' '\t'|sort|uniq > PooledNetwork.txt
+cat *_specific_module.txt|grep -v log10P_Regulation|cut -f 1-3|awk '{$3=$3>0?1:-1}1'|tr ' ' '\t'|sort|uniq > PooledModule.txt
 echo GroupComparison ${Group1} with ${Group2} with PECA_dif_net_multiple done
