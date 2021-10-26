547d73dea5e76c0973b3cf1c907bb0c29be5040c
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2021-10-26 10:18:50 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #/bin/bash
 
 #download the prior data 
-wget http://web.stanford.edu/~zduren/PECA/Thresholding-Based%20SVD_files/Prior.tar.gz
-tar -zxvf Prior.tar.gz
-rm Prior.tar.gz
+cd ./Prior/
+wget -O Opn_median_mm9.bed https://github.com/SUwonglab/PECA/raw/master/Prior/Opn_median_mm9.bed
+wget -O Opn_median_mm10.bed https://github.com/SUwonglab/PECA/raw/master/Prior/Opn_median_mm10.bed
+wget -O Opn_median_hg19.bed https://github.com/SUwonglab/PECA/raw/master/Prior/Opn_median_hg19.bed
+wget -O Opn_median_hg38.bed https://github.com/SUwonglab/PECA/raw/master/Prior/Opn_median_hg38.bed
+wget -O RE_gene_corr_mm9.bed https://github.com/SUwonglab/PECA/raw/master/Prior/RE_gene_corr_mm9.bed
+wget -O RE_gene_corr_mm10.bed https://github.com/SUwonglab/PECA/raw/master/Prior/RE_gene_corr_mm10.bed
+wget -O RE_gene_corr_hg19.bed https://github.com/SUwonglab/PECA/raw/master/Prior/RE_gene_corr_hg19.bed
+wget -O RE_gene_corr_hg38.bed https://github.com/SUwonglab/PECA/raw/master/Prior/RE_gene_corr_hg38.bed
+wget -O TFTG_corr_mouse.mat https://github.com/SUwonglab/PECA/raw/master/Prior/TFTG_corr_mouse.mat
+wget -O TFTG_corr_human.mat https://github.com/SUwonglab/PECA/raw/master/Prior/TFTG_corr_human.mat
+cd ../
 mkdir ./Results/
 mkdir ./Homer/
