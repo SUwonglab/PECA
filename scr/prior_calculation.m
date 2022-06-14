99b9056bbaf63efe78410e9864ed647673a4d893
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2022-06-14 12:29:38 -0400

    Add files via upload

--- scr/prior_calculation.m
+++ scr/prior_calculation.m
@@ -1,26 +1,26 @@
 %%%%%%%load data
-Sample=importdata(SampleNameFile)
+Sample=importdata("SampleNameFile")
 for i=1:size(Sample,1)
-    Exp(:,i)=dlmread(['../../Input/',Sample{i,1},'.txt'],0,1);
-    Opn(:,i)=dlmread(['openness_',Sample{i,1},'.bed'],0,1);
+    fileID = fopen(['../../Input/',Sample{i,1},'.txt']);
+    C = textscan(fileID,'%s %f32');
+    fclose(fileID);
+    Exp(:,i)=C{1,2};
+    Symbol=C{1,1};
+    fileID = fopen(['openness_',Sample{i,1},'.bed']);
+    C = textscan(fileID,'%s %f32');
+    fclose(fileID);
+    Opn(:,i)=C{1,2};
+    PeakName=C{1,1};
 end
 Exp=log2(1+quantilenorm(Exp));
 Opn=log2(1+quantilenorm(Opn));
-fileID = fopen(['../../Input/',Sample{1,1},'.txt']);
-C = textscan(fileID,'%s %*[^\n]');
-fclose(fileID);
-Symbol=C{1,1};
-fileID = fopen(['openness_',Sample{1,1},'.bed']);
-C = textscan(fileID,'%s %*[^\n]');
-fclose(fileID);
-PeakName=C{1,1};
 %%%%%%%%%%%RE_TG
  fileID = fopen('peak_gene.txt');
 C = textscan(fileID,'%s %s');
 fclose(fileID);
 [d f]=ismember(C{1,2},Symbol);
-A1=zeros(length(d),size(A,2));
-A1(d==1,:)=A(f(d==1),:);
+A1=zeros(length(d),size(Exp,2));
+A1(d==1,:)=Exp(f(d==1),:);
 [d f]=ismember(C{1,1},PeakName);
 Opn1=Opn(f,:);
 Z1=zscore(A1');
