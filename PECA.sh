eb0015f7b8ec2c58c1d82ef82793819a776bc75f
Zhana Duren <36247822+durenzn@users.noreply.github.com>
2023-05-20 20:29:46 -0400

    Update PECA.sh

--- PECA.sh
+++ PECA.sh
@@ -67,7 +67,6 @@ cat ../../Prior/TFTG_corr_${speciesFull}.mat > TFTG_corr.mat
 
 echo step 5: Network....
 cp ../../scr/mfbs.m ./.
-cat ../../Prior/TFTG_corr_${speciesFull}.mat > TFTG_corr.mat
 sed "s/toreplace/${input}/g" ../../scr/PECA_network_${genome}.m > PECA_network.m
 module load matlab
 matlab -nodisplay -nosplash -nodesktop -r "PECA_network; exit"
